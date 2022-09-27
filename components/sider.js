830387d47586ddb2adda33caf0903607c0f04e60
obaidtariq <iamobaidtariq@gmail.com>
2022-09-27 09:50:21 -0700

    changes

--- components/sider.js
+++ components/sider.js
@@ -6,182 +6,166 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 const { Sider } = Layout;
 const SideBar = ({ collapsed }) => {
-	const router = useRouter();
-	const items = [
-		{
-			label: "Home",
-			key: "home",
-			icon: (
-				<i className="fa-solid fa-house-user  ant-menu-item-icon"></i>
-			),
-			onClick: () => {
-				router.push("/home");
-			},
-		}, // remember to pass the key prop
-		{
-			label: "Test",
-			icon: (
-				<i className="fa-solid fa-calendar-check ant-menu-item-icon"></i>
-			),
-			key: "test",
-			onClick: () => {
-				router.push("/test");
-			},
-		}, // which is required
-		{
-			label: "Questions",
-			icon: (
-				<i className="fa-solid fa-cube ant-menu-item-icon"></i>
-			),
-			key: "questions",
-			onClick: () => {
-				alert("Questions");
-			},
-		},
-		{
-			label: "Media Library",
-			icon: (
-				<i className="fa-solid fa-image ant-menu-item-icon"></i>
-			),
-			key: "mediaLibrary",
-			onClick: () => {
-				alert("Media Library");
-			},
-		},
-		{
-			label: "Categories",
-			icon: (
-				<i className="fa-solid fa-list ant-menu-item-icon"></i>
-			),
-			key: "categories",
-			onClick: () => {
-				alert("Categories");
-			},
-		},
-		{
-			label: "Admin",
-			key: "admin",
-			icon: (
-				<i className="fa-solid fa-user-shield ant-menu-item-icon"></i>
-			),
-			children: [
-				{
-					label: (
-						<>
-							<i
-								className="fa-solid fa-user-group"
-								style={{
-									padding: "5px",
-								}}
-							></i>
-							Tenants
-						</>
-					),
-					key: "tenants",
-					onClick: () => {
-						alert("tenants");
-					},
-				},
-				{
-					label: (
-						<>
-							{" "}
-							<i
-								className="fa-solid fa-user-group"
-								style={{
-									padding: "5px",
-								}}
-							></i>{" "}
-							Users
-						</>
-					),
-					key: "users",
-					onClick: () => {
-						alert("users");
-					},
-				},
-				{
-					label: (
-						<>
-							{" "}
-							<i
-								className="fa-solid fa-users"
-								style={{
-									padding: "5px",
-								}}
-							></i>{" "}
-							Roles
-						</>
-					),
-					key: "roles",
-					onClick: () => {
-						alert("roles");
-					},
-				},
-				{
-					label: "Exams",
-					label: (
-						<>
-							{" "}
-							<i
-								className="fa-sharp fa-solid fa-file-pen"
-								style={{
-									padding: "5px",
-								}}
-							></i>{" "}
-							Exams
-						</>
-					),
-					key: "exams",
-					onClick: () => {
-						alert("exams");
-					},
-				},
-			],
-		},
-		{
-			label: "Knowledge Base",
-			icon: (
-				<i className="fa-sharp fa-solid fa-circle-question ant-menu-item-icon"></i>
-			),
-			key: "knowledgeBase",
-			onClick: () => {
-				alert("knowledgeBase");
-			},
-		},
-	];
-	return (
-		<Sider trigger={null} collapsible collapsed={collapsed}>
-			<div className="logo">
-				<h3
-					style={{
-						color: "#fff",
-					}}
-				>
-					{collapsed ? (
-						<Image
-							src={Logo}
-							alt="Image logo"
-							width={50}
-							height={50}
-						/>
-					) : (
-						<>
-							<Image
-								src={Logo}
-								alt="Image logo"
-								width={50}
-								height={50}
-							/>{" "}
-							CYGEN SOLUTION
-						</>
-					)}
-				</h3>
-			</div>
-			<hr />
-			<Menu items={items} theme="dark" mode="inline" />
-		</Sider>
-	);
+  const router = useRouter();
+  const items = [
+    {
+      label: "Home",
+      key: "home",
+      icon: <i className="fa-solid fa-house-user  ant-menu-item-icon"></i>,
+      onClick: () => {
+        router.push("/home");
+      },
+    }, // remember to pass the key prop
+    {
+      label: "Test",
+      icon: <i className="fa-solid fa-calendar-check ant-menu-item-icon"></i>,
+      key: "test",
+      onClick: () => {
+        router.push("/test");
+      },
+    }, // which is required
+    {
+      label: "Questions",
+      icon: <i className="fa-solid fa-cube ant-menu-item-icon"></i>,
+      key: "questions",
+      onClick: () => {
+        alert("Questions");
+      },
+    },
+    {
+      label: "Media Library",
+      icon: <i className="fa-solid fa-image ant-menu-item-icon"></i>,
+      key: "mediaLibrary",
+      onClick: () => {
+        alert("Media Library");
+      },
+    },
+    {
+      label: "Categories",
+      icon: <i className="fa-solid fa-list ant-menu-item-icon"></i>,
+      key: "categories",
+      onClick: () => {
+        alert("Categories");
+      },
+    },
+    {
+      label: "Admin",
+      key: "admin",
+      icon: <i className="fa-solid fa-user-shield ant-menu-item-icon"></i>,
+      children: [
+        {
+          label: (
+            <>
+              <i
+                className="fa-solid fa-user-group"
+                style={{
+                  padding: "5px",
+                }}
+              ></i>
+              Tenants
+            </>
+          ),
+          key: "tenants",
+          onClick: () => {
+            alert("tenants");
+          },
+        },
+        {
+          label: (
+            <>
+              {" "}
+              <i
+                className="fa-solid fa-user-group"
+                style={{
+                  padding: "5px",
+                }}
+              ></i>{" "}
+              Users
+            </>
+          ),
+          key: "users",
+          onClick: () => {
+            alert("users");
+          },
+        },
+        {
+          label: (
+            <>
+              {" "}
+              <i
+                className="fa-solid fa-users"
+                style={{
+                  padding: "5px",
+                }}
+              ></i>{" "}
+              Roles
+            </>
+          ),
+          key: "roles",
+          onClick: () => {
+            alert("roles");
+          },
+        },
+        {
+          label: "Exams",
+          label: (
+            <>
+              {" "}
+              <i
+                className="fa-sharp fa-solid fa-file-pen"
+                style={{
+                  padding: "5px",
+                }}
+              ></i>{" "}
+              Exams
+            </>
+          ),
+          key: "exams",
+          onClick: () => {
+            alert("exams");
+          },
+        },
+      ],
+    },
+    {
+      label: "Knowledge Base",
+      icon: (
+        <i className="fa-sharp fa-solid fa-circle-question ant-menu-item-icon"></i>
+      ),
+      key: "knowledgeBase",
+      onClick: () => {
+        alert("knowledgeBase");
+      },
+    },
+  ];
+  return (
+    <Sider
+      trigger={null}
+      collapsible
+      collapsed={collapsed}
+      width={230}
+      //   style={{ width: "230px" }}
+    >
+      <div className="logo">
+        <h3
+          style={{
+            color: "#fff",
+          }}
+        >
+          {collapsed ? (
+            <Image src={Logo} alt="Image logo" width={50} height={50} />
+          ) : (
+            <>
+              <Image src={Logo} alt="Image logo" width={50} height={50} /> CYGEN
+              SOLUTION
+            </>
+          )}
+        </h3>
+      </div>
+      <hr />
+      <Menu items={items} theme="dark" mode="inline" />
+    </Sider>
+  );
 };
 
 export default SideBar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
 import "../styles/globals.css";
 import "antd/dist/antd.css";
 import {
-	PieChartOutlined,
-	DesktopOutlined,
-	FileOutlined,
-	TeamOutlined,
-	UserOutlined,
-	MenuUnfoldOutlined,
-	MenuFoldOutlined,
+  PieChartOutlined,
+  DesktopOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
 } from "@ant-design/icons";
 import { Button, Col, Dropdown, Layout, Menu, Row, Space } from "antd";
 import React, { useEffect, useState } from "react";
@@ -17,109 +17,110 @@ import LoginPage from "./index";
 import { useRouter } from "next/router";
 const { Header, Sider, Content } = Layout;
 const menu = (
-	<Menu
-		items={[
-			{
-				key: "1",
-				label: "Profile",
-			},
-			{
-				key: "2",
-				label: "Logout",
-			},
-		]}
-	/>
+  <Menu
+    items={[
+      {
+        key: "1",
+        label: "Profile",
+      },
+      {
+        key: "2",
+        label: "Logout",
+      },
+    ]}
+  />
 );
 function MyApp({ Component, pageProps }) {
-	const [collapsed, setCollapsed] = useState(false);
-	const [login, setlogin] = useState(false);
-	const router = useRouter();
-	useEffect(() => {
-		if (login) {
-			router.push("/home");
-		} else {
-			router.push("/");
-		}
-	}, [login]);
+  const [collapsed, setCollapsed] = useState(false);
+  const [login, setlogin] = useState(false);
+  const router = useRouter();
+  useEffect(() => {
+    if (login) {
+      router.push("/home");
+    } else {
+      router.push("/");
+    }
+  }, [login]);
 
-	return (
-		<>
-			<Head>
-				<link
-					rel="stylesheet"
-					href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-					integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-					crossorigin="anonymous"
-					referrerpolicy="no-referrer"
-				/>
-			</Head>
-			{login ? (
-				<Layout style={{ height: "100vh" }}>
-					<SideBar collapsed={collapsed} />
-					<Layout className="site-layout">
-						<Header
-							className="site-layout-background"
-							style={{
-								padding: 0,
-							}}
-						>
-							{React.createElement(
-								collapsed
-									? MenuUnfoldOutlined
-									: MenuFoldOutlined,
-								{
-									className: "trigger",
-									onClick: () =>
-										setCollapsed(
-											!collapsed
-										),
-								}
-							)}
-							<div
-								style={{
-									display: "inline",
-									float: "right",
-									marginRight:
-										"5px",
-								}}
-							>
-								<Space size={5}>
-									<Button shape="circle">
-										<i class="fa-sharp fa-solid fa-bell"></i>
-									</Button>
-									<Dropdown
-										overlay={
-											menu
-										}
-										placement="bottomLeft"
-										arrow
-									>
-										<Button shape="circle">
-											<i class="fa-solid fa-user"></i>
-										</Button>
-									</Dropdown>
-								</Space>
-							</div>
-						</Header>
-						<Content
-							className="site-layout-background"
-							style={{
-								margin: "24px 16px",
-								padding: 24,
-								minHeight: 280,
-							}}
-						>
-							<Component
-								{...pageProps}
-							/>
-						</Content>
-					</Layout>
-				</Layout>
-			) : (
-				<LoginPage setlogin={setlogin} />
-			)}
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+          integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+      </Head>
+      {login ? (
+        <Layout style={{ height: "100vh" }}>
+          <SideBar collapsed={collapsed} />
+          <Layout className="site-layout">
+            <Header
+              className="site-layout-background"
+              style={{
+                padding: 0,
+              }}
+            >
+              <React.Fragment>
+                {collapsed ? (
+                  <i
+                    class="fa-solid fa-bars fa-xl"
+                    style={{ marginLeft: "15px" }}
+                    onClick={() => setCollapsed(!collapsed)}
+                  ></i>
+                ) : (
+                  <i
+                    class="fa-solid fa-bars fa-xl"
+                    style={{ marginLeft: "15px" }}
+                    onClick={() => setCollapsed(!collapsed)}
+                  ></i>
+                )}
+              </React.Fragment>
+              {/* {React.createElement(
+                collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+                {
+                  className: "trigger",
+                  onClick: () => setCollapsed(!collapsed),
+                }
+              )} */}
+              <div
+                style={{
+                  display: "inline",
+                  float: "right",
+                  marginRight: "20px",
+                }}
+              >
+                <Space size={10}>
+                  <Button shape="circle">
+                    <i class="fa-sharp fa-solid fa-bell"></i>
+                  </Button>
+                  <Dropdown overlay={menu} placement="bottomLeft" arrow>
+                    <Button shape="circle">
+                      <i class="fa-solid fa-user"></i>
+                    </Button>
+                  </Dropdown>
+                </Space>
+              </div>
+            </Header>
+            <Content
+              className="site-layout-background"
+              style={{
+                margin: "24px 16px",
+                padding: 24,
+                minHeight: 280,
+              }}
+            >
+              <Component {...pageProps} />
+            </Content>
+          </Layout>
+        </Layout>
+      ) : (
+        <LoginPage setlogin={setlogin} />
+      )}
+    </>
+  );
 }
 
 export default MyApp;
